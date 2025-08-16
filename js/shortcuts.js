00a77b92ab43cf648bea429f7a22f891cf5054f5
Teamcanadapro <Dmitri.ivanenko@gmail.com>
2025-08-16 11:16:02 -0700

    Update shortcuts.js
    added mac support for the meta key (CMD)

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -1,16 +1,19 @@
 document.addEventListener('keydown', (e) => {
   const key = e.key.toLowerCase();
 
-  if (e.ctrlKey && e.altKey) {
+  // Support both Windows (Ctrl+Alt) and Mac (Meta+Alt)
+  const ctrlOrCmd = e.ctrlKey || e.metaKey;
+
+  if (ctrlOrCmd && e.altKey) {
     switch (key) {
       case 'm':
         e.preventDefault();
         window.toggleSideMenu?.();
         break;
-     case 'c':
-  e.preventDefault();
-  window.openCalculator?.();
-  break;
+      case 'c':
+        e.preventDefault();
+        window.openCalculator?.();
+        break;
       case 'l':
         e.preventDefault();
         document.getElementById('darkModeToggle')?.click();
@@ -23,12 +26,10 @@ document.addEventListener('keydown', (e) => {
         e.preventDefault();
         window.toggleHelpOverlay?.();
         break;
-    case 'g':
-  e.preventDefault();
-  window.toggleHighReturn?.();
-  break;
-
+      case 'g':
+        e.preventDefault();
+        window.toggleHighReturn?.();
+        break;
     }
   }
 });
-
